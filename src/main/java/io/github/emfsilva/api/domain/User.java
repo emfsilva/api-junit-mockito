cc8478438ed8607863ec182c97c00bcd70a51c25
Emerson Ferreira <emersonfdasilva2@gmail.com>
2022-01-25 20:39:34 -0300

    Classe User Domain

---
+++ src/main/java/io/github/emfsilva/api/domain/User.java
@@ -0,0 +1,39 @@
+package io.github.emfsilva.api.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
