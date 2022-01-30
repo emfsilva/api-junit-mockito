6528e4ea7674d5f14046a49fb0e089f3d448b0a7
Emerson Ferreira <emersonfdasilva2@gmail.com>
2022-01-30 18:48:41 -0300

    Return UserDTO in findById

--- src/main/java/io/github/emfsilva/api/controller/UserController.java
+++ src/main/java/io/github/emfsilva/api/controller/UserController.java
@@ -1,7 +1,8 @@
 package io.github.emfsilva.api.controller;
 
-import io.github.emfsilva.api.domain.User;
+import io.github.emfsilva.api.domain.dto.UserDTO;
 import io.github.emfsilva.api.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,13 +16,16 @@ public class UserController {
 
     private final UserService userService;
 
+    private final ModelMapper mapper;
+
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, ModelMapper mapper) {
         this.userService = userService;
+        this.mapper = mapper;
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(userService.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(mapper.map(userService.findById(id), UserDTO.class));
     }
 }
