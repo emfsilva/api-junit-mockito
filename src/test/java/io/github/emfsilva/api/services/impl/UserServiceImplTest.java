7b2229bb3b0f45a87a26799c57eba23d096197aa
Emerson Ferreira <emersonfdasilva2@gmail.com>
2022-02-01 10:47:13 -0300

    test class user service update method success

--- src/test/java/io/github/emfsilva/api/services/impl/UserServiceImplTest.java
+++ src/test/java/io/github/emfsilva/api/services/impl/UserServiceImplTest.java
@@ -114,8 +114,20 @@ class UserServiceImplTest {
     }
 
     @Test
-    void update() {
+    void whenUpdatehenReturnSuccess() {
+        when(repository.save(any())).thenReturn(user);
+
+        User response = service.update(userDTO);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
     }
+
+
 
     @Test
     void delete() {
