202635facb10a34afae7ed8086d7c9f0e4f58b38
Emerson Ferreira <emersonfdasilva2@gmail.com>
2022-02-03 09:12:09 -0300

    test user controller findAll Success implements

--- src/test/java/io/github/emfsilva/api/controller/UserControllerTest.java
+++ src/test/java/io/github/emfsilva/api/controller/UserControllerTest.java
@@ -10,8 +10,12 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -65,12 +69,24 @@ class UserControllerTest {
     }
 
     @Test
-    void whenFindByIdThenReturnNotFoundException() {
-        when(service.findById(anyInt())).thenReturn(user);
-    }
+    void whenFindAllThenReturnListOfUserDTO() {
+        when(service.findAll()).thenReturn(List.of(user));
+        when(mapper.map(any(), any())).thenReturn(userDTO);
 
-    @Test
-    void findAll() {
+        ResponseEntity<List<UserDTO>> response = resource.findAll();
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(ArrayList.class, response.getBody().getClass());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(UserDTO.class, response.getBody().get(INDEX).getClass());
+
+        assertEquals(ID, response.getBody().get(INDEX).getId());
+        assertEquals(NAME, response.getBody().get(INDEX).getName());
+        assertEquals(EMAIL, response.getBody().get(INDEX).getEmail());
+        assertEquals(PASSWORD, response.getBody().get(INDEX).getPassword());
+
     }
 
     @Test
