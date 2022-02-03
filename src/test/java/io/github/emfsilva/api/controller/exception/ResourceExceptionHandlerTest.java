039b667c56f555a3efb1ee63d84c5630d15a0d63
Emerson Ferreira <emersonfdasilva2@gmail.com>
2022-02-03 16:07:01 -0300

    test class ResourceExceptionHandler method dataIntegrityViolation success  implements

--- src/test/java/io/github/emfsilva/api/controller/exception/ResourceExceptionHandlerTest.java
+++ src/test/java/io/github/emfsilva/api/controller/exception/ResourceExceptionHandlerTest.java
@@ -1,7 +1,7 @@
 package io.github.emfsilva.api.controller.exception;
 
+import io.github.emfsilva.api.services.exceptions.DataIntegrityViolationException;
 import io.github.emfsilva.api.services.exceptions.ObjectNotFoundException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -10,18 +10,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.util.Assert;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.time.LocalDateTime;
+import java.time.Month;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ResourceExceptionHandlerTest {
 
     public static final String USER_NOT_FOUND = "user not found";
+    public static final String E_MAIL_EXIST = "E-mail exist";
+
+
+
     @InjectMocks
     private ResourceExceptionHandler resourceExceptionHandler;
-
 
     @BeforeEach
     void setUp() {
@@ -30,8 +34,11 @@ class ResourceExceptionHandlerTest {
 
     @Test
     void whenObjectNotFoundExceptionThenReturnResponseEntity() {
+
+        LocalDateTime hora = LocalDateTime.of(2016, Month.APRIL, 4, 22, 30);
+
         ResponseEntity<StandardError> response = resourceExceptionHandler
-                .objectNotFound(new ObjectNotFoundException("user not found"), new MockHttpServletRequest());
+                .objectNotFound(new ObjectNotFoundException(USER_NOT_FOUND), new MockHttpServletRequest());
 
         assertNotNull(response);
         assertNotNull(response.getBody());
@@ -40,10 +47,23 @@ class ResourceExceptionHandlerTest {
         assertEquals(StandardError.class, response.getBody().getClass());
         assertEquals(USER_NOT_FOUND, response.getBody().getError());
         assertEquals(404, response.getBody().getStatus());
+        assertNotEquals("/user/2", response.getBody().getPath());
+        assertNotEquals(hora, response.getBody().getTimestamp());
 
     }
 
     @Test
     void dataIntegrityViolation() {
+        ResponseEntity<StandardError> response = resourceExceptionHandler
+                .dataIntegrityViolation(new DataIntegrityViolationException(E_MAIL_EXIST), new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals(E_MAIL_EXIST, response.getBody().getError());
+        assertEquals(400, response.getBody().getStatus());
+
     }
 }
