7c3d43bc3450011ccc7f8b50683fd3082c6705df
Emerson Ferreira <emersonfdasilva2@gmail.com>
2022-02-03 11:06:40 -0300

    test ResourceExceptionHandler method objectNotFound implements

---
+++ src/test/java/io/github/emfsilva/api/controller/exception/ResourceExceptionHandlerTest.java
@@ -0,0 +1,49 @@
+package io.github.emfsilva.api.controller.exception;
+
+import io.github.emfsilva.api.services.exceptions.ObjectNotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.util.Assert;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class ResourceExceptionHandlerTest {
+
+    public static final String USER_NOT_FOUND = "user not found";
+    @InjectMocks
+    private ResourceExceptionHandler resourceExceptionHandler;
+
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void whenObjectNotFoundExceptionThenReturnResponseEntity() {
+        ResponseEntity<StandardError> response = resourceExceptionHandler
+                .objectNotFound(new ObjectNotFoundException("user not found"), new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals(USER_NOT_FOUND, response.getBody().getError());
+        assertEquals(404, response.getBody().getStatus());
+
+    }
+
+    @Test
+    void dataIntegrityViolation() {
+    }
+}
